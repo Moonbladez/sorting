cefe2045b3f46532400aba9494cdeeaf55979c47
Bekki <moonbladez@hotmail.com>
2020-12-24 11:52:16 +0100

    sorting logic implimented

--- build/index.js
+++ build/index.js
@@ -6,8 +6,21 @@ var Sorter = /** @class */ (function () {
     Sorter.prototype.sort = function () {
         //length of collection
         var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            //second loop minus value at the end as that will be the highest
+            for (var j = 0; j < length - i - i; j++) {
+                //compare left hand side to right hand side
+                if (this.collection[j] > this.collection[j + 1]) {
+                    //swap numbers
+                    var leftHandSide = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHandSide;
+                }
+            }
+        }
     };
     return Sorter;
 }());
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,22 @@ class Sorter {
   sort(): void {
     //length of collection
     const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      //second loop minus value at the end as that will be the highest
+      for (let j = 0; j < length - i - i; j++) {
+        //compare left hand side to right hand side
+        if (this.collection[j] > this.collection[j + 1]) {
+          //swap numbers
+          const leftHandSide = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHandSide;
+        }
+      }
+    }
   }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
+console.log(sorter.collection);
