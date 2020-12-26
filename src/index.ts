6927afcdaba44da0832cfb03653d034e2ce24b5a
Bekki <moonbladez@hotmail.com>
2020-12-26 21:04:45 +0100

    added sorter for strings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharacterCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 8, 50, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 8, 50, 0, -100]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("This Is My New String!");
+const sorter = new Sorter(charactersCollection);
+
 sorter.sort();
-
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
