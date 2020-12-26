46668bf245399e9186fcc2a5af0e4b438544bd28
Bekki <moonbladez@hotmail.com>
2020-12-26 17:07:29 +0100

    added build folder to ignore extracted comparisons into seperate file to make sorter generic

--- build/index.js
+++ build/index.js
@@ -1,31 +1,8 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        //length of collection
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            //second loop minus value at the end as that will be the highest
-            for (var j = 0; j < length - i - i; j++) {
-                //compare left hand side to right hand side
-                if (this.collection instanceof Array) {
-                    //if collection === number [0]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        //if collection is Arr of numbers
-                        //swap numbers
-                        var leftHandSide = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHandSide;
-                    }
-                }
-                //if collection is string
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 8, 50, 0]);
+var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-  sort(): void {
-    //length of collection
-    const { length } = this.collection;
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-    for (let i = 0; i < length; i++) {
-      //second loop minus value at the end as that will be the highest
-      for (let j = 0; j < length - i - i; j++) {
-        //compare left hand side to right hand side
-        if (this.collection instanceof Array) {
-          //if collection === number [0]
-          if (this.collection[j] > this.collection[j + 1]) {
-            //if collection is Arr of numbers
-            //swap numbers
-            const leftHandSide = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHandSide;
-          }
-        }
-
-        //if collection is string
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 8, 50, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+
+console.log(numbersCollection.data);
