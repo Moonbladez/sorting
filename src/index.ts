5c9525b9e0b47689ac313cf12632758de0d374b5
Bekki <moonbladez@hotmail.com>
2020-12-27 11:42:48 +0100

    refactored linked list and characters collection to allow abstract class sorter

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,23 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharacterCollection";
 import { LinkedList } from "./LinkedList";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 8, 50, 0, -100]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([10, 3, -5, 8, 50, 0, -90]);
+// numbersCollection.sort();
 
 // console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection("This Is My New String!");
-// const sorter = new Sorter(charactersCollection);
-
-// sorter.sort();
+// const charactersCollection = new CharactersCollection(
+//   "This Is My New Refactored String"
+// );
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(3);
+linkedList.add(50);
+linkedList.add(0);
+linkedList.add(-403);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
